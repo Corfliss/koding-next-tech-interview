efb3acfe22ec27befdfa36160f869e2fa3264f88
Corfliss <work@corfliss.io>
2026-03-18 13:32:59 +0700

    Make code for the website

---
+++ task-website/script.js
@@ -0,0 +1,15 @@
+// 1. Contact Button
+// Clicking the button shows a message
+
+// Get the button needed to be detected
+const contactBtn = document.getElementById("contact-btn");
+
+// Listen to the click event
+contactBtn.addEventListener('click', function () {
+
+  // alert() pops up a message for being clicked
+  alert(
+    'I have been clicked!\n' + 
+    'Muehehehehhehehehe'
+  );
+});
